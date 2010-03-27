2203a8068ba69b225327bab173d9d6c74408b829
Foo Bar <argriffi@qsl.nc.rr.com>
2010-03-27 17:21:19 -0400

    finish the test pattern code

--- Makefile
+++ Makefile
@@ -1,22 +1,27 @@
 FLAGS = -Wall -Wextra -pedantic -pipe -march=native
 
 .PHONY: all
-all : downsample-box demux elementwise-sum
+all : downsample-box demux elementwise-sum write-test-pattern
 
 downsample-box : downsample-box.o
 	gcc downsample-box.o -o downsample-box
 downsample-box.o : downsample-box.c
-	gcc $(FLAGS) -c downsample-box.c
+	gcc $(FLAGS) -std=c99 -c downsample-box.c
 
 demux : demux.o
 	gcc demux.o -o demux
 demux.o : demux.c
-	gcc $(FLAGS) -c demux.c
+	gcc $(FLAGS) -std=c99 -c demux.c
 
 elementwise-sum : elementwise-sum.o
 	g++ elementwise-sum.o -o elementwise-sum
 elementwise-sum.o : elementwise-sum.C elementwise-sum.H
 	g++ $(FLAGS) -c elementwise-sum.C
+
+write-test-pattern : write-test-pattern.o
+	g++ write-test-pattern.o -o write-test-pattern
+write-test-pattern.o : write-test-pattern.C write-test-pattern.H
+	g++ $(FLAGS) -c write-test-pattern.C
 
 clean :
 	rm *.o
--- write-test-pattern.C
+++ write-test-pattern.C
@@ -10,83 +10,107 @@ int main(int argc, char *argv[])
 {
   int errcode = EXIT_SUCCESS;
   /* read the flag options */
-  char typechar;
-  char widthchar;
-  int nreps;
+  char typechar = 'd';
+  char widthchar = '4';
+  int width = 4;
+  int nreps = 1;
+  int verbose_flag = 0;
   static struct option long_options[] =
   {
+    {"verbose", no_argument, &verbose_flag, 1},
     {"format", required_argument, 0, 'f'},
-    {"nreps", required_argument, 0, 'r'},
+    {"nreps", required_argument, 0, 'n'},
     {0, 0, 0, 0}
   };
-  int option_index = 0;
-  int c = getopt_long(argc, argv, "f:r:", long_options, &option_index);
-  switch (c)
+  while (1)
   {
-    case '?':
-      return EXIT_FAILURE;
-    case 'f':
-      if (strlen(optarg) != 2)
-      {
-        fprintf(stderr, "the type string should be two characters\n");
-        return EXIT_FAILURE;
-      }
-      typechar = optarg[0];
-      widthchar = optarg[1];
-      if (typechar != 'd' && typechar != 'f')
-      {
-        fprintf(stderr, "the first format character should be 'd' or 'f'\n");
-        return EXIT_FAILURE;
-      }
-      if (widthchar != '4' && widthchar != '8')
-      {
-        fprintf(stderr, "the second format character should be '4' or '8'\n");
-        return EXIT_FAILURE;
-      }
+    int option_index = 0;
+    int c = getopt_long(argc, argv, "vf:n:", long_options, &option_index);
+    if (c == -1)
       break;
-    default:
-      fprintf(stderr, "unknown format\n");
-      return EXIT_FAILURE;
+    switch (c)
+    {
+      case 0:
+        /* process flags and do things on options */
+        break;
+      case 'f':
+        if (strlen(optarg) != 2)
+        {
+          fprintf(stderr, "the type string should be two characters\n");
+          return EXIT_FAILURE;
+        }
+        typechar = optarg[0];
+        widthchar = optarg[1];
+        if (typechar != 'd' && typechar != 'f')
+        {
+          fprintf(stderr, "the first format character should be 'd' or 'f'\n");
+          return EXIT_FAILURE;
+        }
+        switch (widthchar)
+        {
+          case '4':
+            width = 4;
+            break;
+          case '8':
+            width = 8;
+            break;
+          default:
+            fprintf(stderr, "the second format char should be '4' or '8'\n");
+            return EXIT_FAILURE;
+        }
+        break;
+      case 'n':
+        nreps = atoi(optarg);
+        if (nreps < 1)
+        {
+          fprintf(stderr, "expected a positive number of repeats\n");
+          return EXIT_FAILURE;
+        }
+        break;
+      case '?':
+        return EXIT_FAILURE;
+      default:
+        return EXIT_FAILURE;
+    }
   }
-  if (optind >= argc)
-  {
-    fprintf(stderr, "expected a filename\n");
-    return EXIT_FAILURE;
-  }
-  /* open the output file for writing binary data */
-  FILE *fout = fopen(argv[optind], "wb");
-  if (!fout)
-  {
-    fprintf(stderr, "Failed to open %s for writing.\n", argv[optind]);
-    return EXIT_FAILURE;
-  }
-  /* open the input files for reading the binary data */
+  /* allocate memory for the pattern */
+  if (verbose_flag)
+    fprintf(stderr, "allocate pattern...\n");
+  int npattern = argc - optind;
+  void *pattern = malloc(npattern * width);
   int i;
-  int ninputs = argc-3;
-  FILE **infiles = (FILE **) malloc(ninputs*sizeof(FILE*));
-  for (i=0; i<ninputs; i++) infiles[i] = NULL;
-  for (i=0; i<ninputs; i++)
+  /* write the pattern */
+  if (verbose_flag)
   {
-    infiles[i] = fopen(argv[optind+1+i], "rb");
+    fprintf(stderr, "write the pattern using ");
+    fprintf(stderr, "nreps=%d npattern=%d...\n", nreps, npattern);
   }
-  /* do the summation */
-  if (typechar == 'd' && widthchar == '4')
+  if (typechar == 'd' && width == 4)
   {
-    if (process<int32_t>(ninputs, infiles, fout) < 0) errcode = EXIT_FAILURE;
-  } else if (typechar == 'd' && widthchar == '8') {
-    if (process<int64_t>(ninputs, infiles, fout) < 0) errcode = EXIT_FAILURE;
-  } else if (typechar == 'f' && widthchar == '4') {
-    if (process<float>(ninputs, infiles, fout) < 0) errcode = EXIT_FAILURE;
-  } else if (typechar == 'f' && widthchar == '8') {
-    if (process<double>(ninputs, infiles, fout) < 0) errcode = EXIT_FAILURE;
+    for (i=0; i<npattern; i++)
+      ((int32_t *) pattern)[i] = (int32_t) atol(argv[optind + i]);
+    if (process<int32_t>(nreps, npattern, (int32_t *) pattern, stdout) < 0)
+      errcode = EXIT_FAILURE;
+  } else if (typechar == 'd' && width == 8) {
+    for (i=0; i<npattern; i++)
+      ((int64_t *) pattern)[i] = (int64_t) atol(argv[optind + i]);
+    if (process<int64_t>(nreps, npattern, (int64_t *) pattern, stdout) < 0)
+      errcode = EXIT_FAILURE;
+  } else if (typechar == 'f' && width == 4) {
+    for (i=0; i<npattern; i++)
+      ((float *) pattern)[i] = (float) atof(argv[optind + i]);
+    if (process<float>(nreps, npattern, (float *) pattern, stdout) < 0)
+      errcode = EXIT_FAILURE;
+  } else if (typechar == 'f' && width == 8) {
+    for (i=0; i<npattern; i++)
+      ((double *) pattern)[i] = (double) atof(argv[optind + i]);
+    if (process<double>(nreps, npattern, (double *) pattern, stdout) < 0)
+      errcode = EXIT_FAILURE;
   }
   /* clean up */
-  if (fout) fclose(fout);
-  for (i=0; i<ninputs; i++)
-  {
-    if (infiles[i]) fclose(infiles[i]);
-  }
-  free(infiles);
+  if (verbose_flag)
+    fprintf(stderr, "cleaup...\n");
+  free(pattern);
   return errcode;
 }
 
